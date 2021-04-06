943b5d50f84fd75bb3e103176009c7ea908596e7
campper <wichapon@frontware.co.th>
2021-04-06 16:35:58 +0700

    dev 06042021 1635

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -52,10 +52,6 @@ class CustomAppBar extends StatelessWidget {
             title: "Login",
             press: () {},
           ),
-          DefaultButton(
-            text: "Get Started",
-            press: () {},
-          ),
         ],
       ),
     );
@@ -63,13 +59,28 @@ class CustomAppBar extends StatelessWidget {
 }
 
 class MenuItem extends StatelessWidget {
+  final String title;
+  final Function press;
   const MenuItem({
     Key key,
+    this.title,
+    this.press,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Text(
+          title.toUpperCase(),
+          style: TextStyle(
+            color: Colors.grey.withOpacity(0.3),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
