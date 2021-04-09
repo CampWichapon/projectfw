453bccf94cd527fe564eb406ec1311e5a3ecf0ba
campper <wichapon@frontware.co.th>
2021-04-09 17:13:19 +0700

    dev 09042021 1713

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,12 +1,20 @@
 import 'dart:html';
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:projectfw/theme_profile.dart';
+
+import 'package:projectfw/components/navbar.dart';
+import 'package:projectfw/components/drawer.dart';
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
+      appBar: Navbar(
+        title: "Profile",
+      ),
     );
   }
 }
